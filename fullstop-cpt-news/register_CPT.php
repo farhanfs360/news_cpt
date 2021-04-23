909fc34ed15de86e31e0a8e17c193331e34dddce
farhanfs360 <farhan.fs360@gmail.com>
2021-04-23 11:06:16 +0500

    full plugin

---
+++ fullstop-cpt-news/register_CPT.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+
+Plugin Name: Fullstop360 Register Post Types
+
+Plugin URI: https://fullstop360.com/
+
+Description: This plugin is created for Custom Post Type for News by Fullstop
+
+Version: 1.0
+
+Author: Farhan Khan
+
+Author URI: https://fullstop360.com/
+
+License: GPLv2 or later
+
+Text Domain: Fullstop
+
+*/
+
+include( plugin_dir_path( __FILE__ ) . 'includes/news_cpt.php' );
